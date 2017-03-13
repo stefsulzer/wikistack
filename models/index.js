e452802acf460b975968020540b24039fddf876f
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-03-13 11:44:53 -0400

    Updated to Getter Method

--- models/index.js
+++ models/index.js
@@ -23,8 +23,14 @@ var Page = db.define('page', {
     date: {
         type: Sequelize.DATE,
         defaultValue: Sequelize.NOW
-    }
-});
+    }},
+    {
+      getterMethods: {
+        route: function() {
+          return '/wiki/' + this.urlTitle;
+        }
+      }
+  });
 
 var User = db.define('user', {
     name: {
@@ -47,3 +53,4 @@ module.exports = {
   Page: Page,
   User: User
 };
+
